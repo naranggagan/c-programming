0ce14fd95e3e1e107cd31792fdf277011e1fb445
naranggagan <62908212+naranggagan@users.noreply.github.com>
2020-08-15 00:47:48 +0530

    Update retirement.c

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -51,7 +51,7 @@ int main()
 {
 int startAge=327;
 double initial=21345;
-retire_info working={489 , 1000.0 , (0.04/12)};
+retire_info working={489 , 1000.0 , 0.00375};
 retire_info retired={384 , -4000.0 , (0.01/12)};
 
 retirement(startAge, initial, working, retired);
