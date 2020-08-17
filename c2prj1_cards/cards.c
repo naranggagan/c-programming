b533fe4e6d8d72a6bc946d5ef112f2e173a2cb62
Gagan Narang <62908212+naranggagan@users.noreply.github.com>
2020-08-17 09:03:41 +0530

    Update cards.c

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,16 +5,33 @@
 
 
 void assert_card_valid(card_t c) {
-
+assert((c.value>=2 && c.value<=VALUE_ACE) && (c.suit>=SPADES && c.suit<=CLUBS));
 
 }
 
 const char * ranking_to_string(hand_ranking_t r) {
-  return "";
+  switch(r){
+  case STRAIGHT_FLUSH: return "STRAIGHT_FLUSH";
+  case FOUR_OF_A_KIND: return "FOUR_OF_A_KIND";
+  case FULL_HOUSE: return "FULL_HOUSE";
+  case FLUSH: return "FLUSH";
+  case STRAIGHT: return "STRAIGHT";
+  case THREE_OF_A_KIND: return "THREE_OF_A_KIND";
+  case TWO_PAIR: return "TWO_PAIR";
+  case PAIR: return "PAIR";
+  case NOTHING: return "NOTHING";
+  default: return "";
 }
 
 char value_letter(card_t c) {
-  return 'x';
+  char x='\0';
+  if(c.value>=2 && c.value<=9) { x='0'+c.value; }
+  else if(c.value==10) { x='0'; }
+  else if(c.value==VALUE_JACK) { x='J'; }
+  else if(c.value==VALUE_QUEEN) { x='Q'; }
+  else if(c.value==VALUE_KING) { x='K'; }
+  else if(c.value==VALUE_ACE) { x='A'; }
+  return x;
 }
 
 
