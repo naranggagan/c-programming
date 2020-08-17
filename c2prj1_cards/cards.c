30ec586d4961a21aae4793d2d320fede2ca07b4d
Gagan Narang <62908212+naranggagan@users.noreply.github.com>
2020-08-17 15:19:29 +0530

    Update cards.c

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,7 +5,7 @@
 
 
 void assert_card_valid(card_t c) {
-assert((c.value>=2 && c.value<=VALUE_ACE) && (c.suit>=SPADES && c.suit<=CLUBS));
+assert((c.value >= 2 && c.value <= VALUE_ACE) && (c.suit >= SPADES && c.suit <= CLUBS));
 
 }
 
@@ -27,11 +27,11 @@ const char * ranking_to_string(hand_ranking_t r) {
 char value_letter(card_t c) {
   \*char x='0';
   if(c.value>=2 && c.value<=9) { x='0'+c.value; }
-  else if(c.value==10) { x='0'; }
-  else if(c.value==VALUE_JACK) { x='J'; }
-  else if(c.value==VALUE_QUEEN) { x='Q'; }
-  else if(c.value==VALUE_KING) { x='K'; }
-  else if(c.value==VALUE_ACE) { x='A'; }
+  else if(c.value == 10) { x='0'; }
+  else if(c.value == VALUE_JACK) { x='J'; }
+  else if(c.value == VALUE_QUEEN) { x='Q'; }
+  else if(c.value == VALUE_KING) { x='K'; }
+  else if(c.value == VALUE_ACE) { x='A'; }
   return x;
 }
 
@@ -52,20 +52,21 @@ printf("%c%c",value_letter(c),suit_letter(c));
 
 card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
-  if(value_let>='2' && value_let<='9') { temp.value=value_let-'0'; }
-  else if(value_let=='0') { temp.value=10; }
-  else if(value_let=='J') { temp.value=VALUE_JACK; }
-  else if(value_let=='Q') { temp.value=VALUE_QUEEN; }
-  else if(value_let=='K') { temp.value=VALUE_KING; }
-  else if(value_let=='A') { temp.value=VALUE_ACE; }
-  else { assert((value_let>='2' && value_let<='9') && value_let=='0' && value_let=='J' && value_let=='Q' && value_let=='K' && value_let=='A'); }
+  if(value_let>='2' && value_let<='9')
+  {temp.value = value_let-'0';}
+  else if(value_let == '0') { temp.value=10; }
+  else if(value_let == 'J') { temp.value=VALUE_JACK; }
+  else if(value_let == 'Q') { temp.value=VALUE_QUEEN; }
+  else if(value_let == 'K') { temp.value=VALUE_KING; }
+  else if(value_let == 'A') { temp.value=VALUE_ACE; }
+  else { assert((value_let >= '2' && value_let <= '9') && value_let == '0' && value_let == 'J' && value_let == 'Q' && value_let == 'K' && value_let == 'A'); }
   
   switch(suit_let){
-  case 's': temp.suit=SPADES; break;
-  case 'h': temp.suit=HEARTS; break;
-  case 'd': temp.suit=DIAMONDS; break;
-  case 'c': temp.suit=CLUBS; break;
-  default: assert(suit_let=='s' && suit_let=='h' && suit_let=='d' && suit_let=='c');
+  case 's': temp.suit = SPADES; break;
+  case 'h': temp.suit = HEARTS; break;
+  case 'd': temp.suit = DIAMONDS; break;
+  case 'c': temp.suit = CLUBS; break;
+  default: assert(suit_let == 's' && suit_let == 'h' && suit_let == 'd' && suit_let == 'c');
   }
   return temp;
 }
